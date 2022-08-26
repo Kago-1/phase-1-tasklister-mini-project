db7fb2137878568d576f857c08774d6f6f44bef5
Stephen <stephenkago10@gmail.com>
2022-08-27 01:02:15 +0300

    Task Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleTask(e.target.newTask.value);
+  });
+
+  function handleTask(task) {
+    let li = document.createElement("li");
+    let del = document.createElement("button");
+    li.innerHTML = task;
+    document.querySelector("ul").appendChild(li);
+    document.querySelector("li").appendChild(del);
+    document.querySelector("button").addEventListener("click", () => {
+      document.querySelector("li").remove();
+    });
+  }
 });
